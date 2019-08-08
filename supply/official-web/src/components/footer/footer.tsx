9bef713f9304700ee8b4233500c75c69d64267db
Lu Zong <415390588@qq.com>
2019-08-09 01:12:08 +0800

    commit

---
+++ supply/official-web/src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "antd/es/button";
+
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <p>Hello World</p>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
+
+export default Footer;
--- supply/official-web/src/index.tsx
+++ supply/official-web/src/index.tsx
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from "antd/es/button";
 import "./index.scss";
-
-const App: React.FC = () => {
-  return (
-    <div>
-      <p>Hello World</p>
-      <Button type="primary">Button</Button>
-    </div>
-  );
-};
+import App from "./app";
 
 ReactDOM.render(<App />, document.getElementById("app"));
