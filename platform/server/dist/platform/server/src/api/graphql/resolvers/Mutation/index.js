5d9ca895d5d49e6b5ea80cee8a7ec72e83a4ff77
Lu Zong <415390588@qq.com>
2019-08-15 00:52:22 +0800

    删除dist文件夹

--- platform/server/dist/platform/server/src/api/graphql/resolvers/Mutation/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mutationResolvers = {};
-exports.default = mutationResolvers;
-//# sourceMappingURL=index.js.map
--- platform/server/dist/platform/server/src/api/graphql/resolvers/Query/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const queryResolvers = {};
-exports.default = queryResolvers;
-//# sourceMappingURL=index.js.map
