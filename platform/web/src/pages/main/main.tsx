ecb2191d432e7b99a19ef6b31e3e5cfdd3056e25
Lu Zong <415390588@qq.com>
2019-08-15 01:47:51 +0800

    commit

--- platform/web/src/pages/main/main.tsx
+++ platform/web/src/pages/main/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
+import { Layout, Menu, Breadcrumb, Row, Col } from "antd";
 const { Header, Footer, Content } = Layout;
 import "./main.scss";
 
@@ -7,16 +7,22 @@ const Main: React.FC = () => {
   return (
     <Layout className="layout">
       <Header className="header">
-        <div className="logo" />
-        <Menu
-          theme="light"
-          mode="horizontal"
-          defaultSelectedKeys={["1"]}
-          className="menu"
-        >
-          <Menu.Item key="1">首页</Menu.Item>
-          <Menu.Item key="2">社区</Menu.Item>
-        </Menu>
+        <Row>
+          <Col span={4}>
+            <div className="logo" />
+          </Col>
+          <Col span={12}>
+            <Menu
+              mode="horizontal"
+              defaultSelectedKeys={["1"]}
+              className="menu"
+            >
+              <Menu.Item key="1">首页</Menu.Item>
+              <Menu.Item key="2">社区</Menu.Item>
+            </Menu>
+          </Col>
+          <Col span={8}>登录 注册</Col>
+        </Row>
       </Header>
       <Content style={{ padding: "0 50px" }}>
         <Breadcrumb style={{ margin: "16px 0" }}>
