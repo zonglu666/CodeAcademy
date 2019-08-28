ba2755fd9f5fa92d464da19167e7bff3cabd4fc6
Lu Zong <415390588@qq.com>
2019-08-28 15:51:04 +0800

    commit

---
+++ platform/web/src/components/body.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./body.scss";
+
+const Component: React.FC = () => {
+  return <div>content</div>;
+};
+
+export default Component;
--- platform/web/src/pages/main.tsx
+++ platform/web/src/pages/main.tsx
@@ -5,19 +5,18 @@ import "./main.scss";
 
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Body from "../components/body";
 
-const Main: React.FC = () => {
+const Page: React.FC = () => {
   return (
     <Layout className="layout">
       <Header></Header>
       <Content style={{ padding: "0 50px" }}>
-        <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-          Content
-        </div>
+        <Body></Body>
       </Content>
       <Footer></Footer>
     </Layout>
   );
 };
 
-export default Main;
+export default Page;
