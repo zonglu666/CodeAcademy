6278502876d11c44aabd9a7217ff22d04d844780
zonglu <zonglu@sheca.com>
2020-07-29 17:57:34 +0800

    commit

--- platform/server/src/index.ts
+++ platform/server/src/index.ts
@@ -1,26 +1,26 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
-import express from "express";
-import mongoose from "mongoose";
-import config from "./config";
-import { ApolloServer } from "apollo-server-express";
-import { typeDefs, resolvers } from "./api/graphql";
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './api/graphql';
 
 mongoose.connect(config.MONGO_URL);
 
 const playground: any = {
   settings: {
-    "editor.theme": "light",
-    "editor.cursorShape": "line"
-  }
+    'editor.theme': 'light',
+    'editor.cursorShape': 'line',
+  },
 };
 
 const server = new ApolloServer({
   playground,
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 const app = express();
